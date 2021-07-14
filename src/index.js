b533da9c90ca088955d5d2a1c1ccb3b4b4cffb62
Arafat Olayiwola <harof.dev@gmail.com>
2021-07-14 07:12:19 +0100

    modified src/index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './Store/Store';
+import store from './Store/store';
 
 
 ReactDOM.render(
